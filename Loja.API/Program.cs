ab5e22f7463595c2e53051e9a94a378513089a0f
mylle <myllersilva1310@gmail.com>
2024-02-11 12:30:44 -0300

    Configuration Program

--- Loja.API/Program.cs
+++ Loja.API/Program.cs
@@ -1,15 +1,21 @@
+using System.Text.Json.Serialization;
 using Loja.Application.Contracts;
 using Loja.Application.Services;
 using Loja.Domain.Contracts;
 using Loja.Infra.Context;
 using Loja.Infra.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,23 +31,39 @@ builder
     .AddScoped<IUsuarioRepository, UsuarioRepository>()
     .AddScoped<IDescontoRepository, DescontoRepository>()
     .AddScoped<ILojaRepository, LojaRepository>()
-    .AddScoped<IProdutoRepository, ProdutoRepository>();
+    .AddScoped<IProdutoRepository, ProdutoRepository>()
+    .AddScoped<IEstoqueRepository, EstoqueRepository>();
 
 builder
     .Services
     .AddScoped<IUsuarioService, UsuarioService>()
     .AddScoped<IDescontoService, DescontoService>()
     .AddScoped<ILojaService, LojaService>()
-    .AddScoped<IProdutoService, ProdutoService>();
+    .AddScoped<IProdutoService, ProdutoService>()
+    .AddScoped<IEstoqueService, EstoqueService>();
 
+// Configuração do Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Loja API",
+        Version = "v1",
+        Description = "API de Gerenciamento de Loja",
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger();// Configurações adicionais do Swagger UI
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Loja API v1");
+    });
 }
 
 app.UseHttpsRedirection();
